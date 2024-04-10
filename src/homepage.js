bf0617316efa4e0f76adebc778ac349a56206b89
David Agredano <davidagredano@gmail.com>
2024-04-11 00:47:51 +0200

    Update headline text styling

--- src/homepage.js
+++ src/homepage.js
@@ -2,7 +2,7 @@ import homepageImage from "./homepage.jpeg";
 
 function Headline() {
   const h1 = document.createElement("h1");
-  h1.textContent = "Savor the Delight at David's Maison Gourmet";
+  h1.textContent = "Home";
   return h1;
 }
 
