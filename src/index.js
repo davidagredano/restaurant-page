27f4d901cf65ee53ab69dbc062e1b31531311409
David Agredano <davidagredano@gmail.com>
2024-05-10 19:50:52 +0200

    Clear previous nav-btn hover effect

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import Menu from "./menu";
 import Contact from "./contact";
 import "./style.css";
 
-const navBtns = document.querySelectorAll(".nav__button");
 const homeBtn = document.querySelector("#home-btn");
 const menuBtn = document.querySelector("#menu-btn");
 const contactBtn = document.querySelector("#contact-btn");
@@ -13,16 +12,6 @@ function replaceContent(component) {
   content.innerHTML = "";
   content.appendChild(component);
 }
-
-navBtns.forEach((btn) => {
-  const lineElement = btn.querySelector(".nav__button-line");
-  btn.addEventListener("mouseover", () => {
-    lineElement.style.width = "100%";
-  });
-  btn.addEventListener("mouseout", () => {
-    lineElement.style.width = "0%";
-  });
-});
 
 homeBtn.addEventListener("click", () => {
   replaceContent(Homepage());
