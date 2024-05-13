70fed0805f843aa1eb09957742520ca3e7d9916e
David Agredano <davidagredano@gmail.com>
2024-05-13 20:38:38 +0200

    Add and style contact section
    This commit introduces a new contact section with contact cards with
    contact data. The CSS file creates a flex container for the contact
    cards, ensuring they are centered and evenly spaced. The JavaScript
    file imports the necessary components and data, dynamically generates
    contact cards with email and phone information, and appends them to
    the contact section.

--- src/contact/index.js
+++ src/contact/index.js
@@ -1,7 +1,34 @@
+import "./index.css";
 import PageHeader from "../components/PageHeader";
+import ContactCard from "./ContactCard";
+import emailIcon from "../components/icons/email.svg";
+import phoneIcon from "../components/icons/phone.svg";
 
 export default function Contact() {
+  const contactCards = [
+    {
+      iconPath: emailIcon,
+      iconAlt: "Mail Icon",
+      heading: "Email Us",
+      text: ["support@biteandsizzle.com", "press@biteandsizzle.com"],
+    },
+    {
+      iconPath: phoneIcon,
+      iconAlt: "Phone Icon",
+      heading: "Call Us",
+      text: ["(+34) 622 658 553", "(+34) 622 658 554"],
+    },
+  ];
+
   const fragment = new DocumentFragment();
   fragment.appendChild(PageHeader("Contact"));
+
+  const contactCardsSection = document.createElement("section");
+  contactCardsSection.classList.add("contact-cards-section");
+  contactCards.forEach((contactCard) => {
+    contactCardsSection.appendChild(ContactCard(contactCard));
+  });
+  fragment.appendChild(contactCardsSection);
+
   return fragment;
 }
