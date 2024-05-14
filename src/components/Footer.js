1ff948186005b6f950efee8e972e48914026ccd3
David Agredano <davidagredano@gmail.com>
2024-05-14 18:02:09 +0200

    Add Footer component with styling and functionality
    This commit introduces a new Footer component, which includes both the
    CSS styling and the JavaScript functionality. The Footer component is
    designed to provide a consistent bottom section for the application,
    featuring a logo, information cards and a ScrollTopButton for improved
    user navigation.
    
    The Footer component enhances the user interface by offering a polished
    and informative section that can be easily added to each page of the
    website.

---
+++ src/components/Footer.js
@@ -0,0 +1,51 @@
+import "./Footer.css";
+import ScrollTopButton from "./ScrollTopButton";
+import InfoCard from "./InfoCard";
+
+function FooterLogo() {
+  const footerLogo = document.createElement("div");
+  const letterB = document.createElement("span");
+  const symbolEt = document.createElement("span");
+  const letterS = document.createElement("span");
+  letterB.textContent = "B";
+  symbolEt.textContent = "&";
+  letterS.textContent = "S";
+  letterB.classList.add("footer__logo");
+  symbolEt.classList.add("footer__logo", "footer__logo--accent");
+  letterS.classList.add("footer__logo");
+  footerLogo.appendChild(letterB);
+  footerLogo.appendChild(symbolEt);
+  footerLogo.appendChild(letterS);
+  return footerLogo;
+}
+
+const infoCards = [
+  {
+    heading: "Find Us",
+    text: ["Carrer del Sabor, 42", "08001 Barcelona, Spain"],
+  },
+  {
+    heading: "When We Sizzle",
+    text: [
+      "Monday to Friday: 11:00 - 22:00",
+      "Saturday & Sunday: 12:00 - 23:00",
+    ],
+  },
+];
+
+export default function Footer() {
+  const footer = document.createElement("footer");
+  footer.classList.add("footer");
+
+  const footerWrapper = document.createElement("div");
+  footerWrapper.classList.add("footer__wrapper");
+  footer.appendChild(footerWrapper);
+
+  footerWrapper.appendChild(FooterLogo());
+  infoCards.forEach((infoCard) => {
+    footerWrapper.appendChild(InfoCard(infoCard));
+  });
+  footerWrapper.appendChild(ScrollTopButton());
+
+  return footer;
+}
