049581fd091f686ca9846cbf1482bb253322d2ac
David Agredano <davidagredano@gmail.com>
2024-05-16 20:45:05 +0200

    Add initial menu content structure
    Introduce provisional content for menu categories and products to
    facilitate further development of the UI. This includes a basic
    list of categories and a detailed array of products with descriptions
    and prices, serving as a starting point for menu implementation.

---
+++ src/menu/content.js
@@ -0,0 +1,104 @@
+export const categories = ["all", "burgers", "sides", "desserts", "drinks"];
+
+export const products = {
+  all: [
+    {
+      src: null,
+      name: "Original Stack",
+      description:
+        "A double layer of juicy beef patties, topped with cheddar and a dollop of our house sauce.",
+      price: "8,99€",
+    },
+  ],
+  burgers: [
+    {
+      src: null,
+      name: "Original Stack",
+      description:
+        "A double layer of juicy beef patties, topped with cheddar and a dollop of our house sauce.",
+      price: "8,99€",
+    },
+    {
+      src: null,
+      name: "BBQ Bacon Burger",
+      description:
+        "Savory beef burger under a blanket of crispy bacon, smothered in BBQ sauce and cheddar.",
+      price: "9,49€",
+    },
+    {
+      src: null,
+      name: "Vegan Truffle",
+      description:
+        "A Beyond burger with a hint of truffle in a creamy veganise, simple yet sophisticated.",
+      price: "9,99€",
+    },
+  ],
+  sides: [
+    {
+      src: null,
+      name: "Wings of Glory",
+      description:
+        "Succulent chicken wings, upsized for the ultimate poultry aficionado. When it’s time to soar, make it with these wings.",
+      price: "5,99€",
+    },
+    {
+      src: null,
+      name: "Golden Nuggets",
+      description:
+        "6 crispy, golden chicken nuggets, perfect for dipping into our rich, flavorful sauce.",
+      price: "4,99€",
+    },
+    {
+      src: null,
+      name: "Supreme Spuds",
+      description:
+        "Elevate your meal with our Supreme Potatoes, and don’t miss out on trying them with our new Sour Cream sauce.",
+      price: "3,99€",
+    },
+  ],
+  desserts: [
+    {
+      src: null,
+      name: "Caramel Bliss Sundae",
+      description:
+        "Indulge in the sweet embrace of vanilla ice cream drizzled with caramel syrup.",
+      price: "4,99€",
+    },
+    {
+      src: null,
+      name: "Strawberry Classic Scoop",
+      description:
+        "Timeless vanilla ice cream meets strawberry syrup for a match made in dessert heaven.",
+      price: "4,99€",
+    },
+    {
+      src: null,
+      name: "Chocolate Dream Drizzle",
+      description:
+        "Vanilla ice cream paired with chocolate syrup, a top-tier classic treat.",
+      price: "4,99€",
+    },
+  ],
+  drinks: [
+    {
+      src: null,
+      name: "Coca-Cola",
+      description:
+        "The iconic Coca-Cola, served ice-cold for that refreshing fizz",
+      price: "2,49€",
+    },
+    {
+      src: null,
+      name: "Monster Energy",
+      description:
+        "Unleash the beast with Monster Energy, for when you need that extra kick.",
+      price: "3,49€",
+    },
+    {
+      src: null,
+      name: "Mineral Water",
+      description: "Crisp and refreshing mineral water to quench your thirst.",
+      price: "1,99€",
+    },
+  ],
+};
