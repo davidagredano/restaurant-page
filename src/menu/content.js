ce29bb0aa7be9cd57cdffa0383020ce2828db6f5
David Agredano <davidagredano@gmail.com>
2024-05-17 17:03:54 +0200

    Update menu content and add product images
    - Add new menu items with descriptions and prices
    - Import new burger, side, dessert, and drink images
    - Update product details in menu content.js to include images
    
    This commit updates the menu content.js file, adding new menu items
    with their descriptions and prices. This expands the variety of choices
    available to customers and ensures that the menu page displays the
    latest offerings with accurate descriptions and pricing.
    
    Additionally, a series of new product images have been imported. These
    images, which include new burgers, sides, desserts, and drinks, are
    referenced alongside each product in the updated menu content.js file.
    The addition of these images enhances the visual appeal of the menu and
    provides customers with a better understanding of what they are ordering

--- src/menu/content.js
+++ src/menu/content.js
@@ -1,9 +1,31 @@
+import burgers1 from "./product-img/burgers1.png";
+import burgers2 from "./product-img/burgers2.png";
+import burgers3 from "./product-img/burgers3.png";
+import burgers4 from "./product-img/burgers4.png";
+import burgers5 from "./product-img/burgers5.png";
+import burgers6 from "./product-img/burgers6.png";
+import burgers7 from "./product-img/burgers7.png";
+import burgers8 from "./product-img/burgers8.png";
+import burgers9 from "./product-img/burgers9.png";
+import sides1 from "./product-img/sides1.png";
+import sides2 from "./product-img/sides2.png";
+import sides3 from "./product-img/sides3.png";
+import sides4 from "./product-img/sides4.png";
+import sides5 from "./product-img/sides5.png";
+import sides6 from "./product-img/sides6.png";
+import desserts1 from "./product-img/desserts1.png";
+import desserts2 from "./product-img/desserts2.png";
+import desserts3 from "./product-img/desserts3.png";
+import drinks1 from "./product-img/drinks1.png";
+import drinks2 from "./product-img/drinks2.png";
+import drinks3 from "./product-img/drinks3.png";
+
 export const categories = ["all", "burgers", "sides", "desserts", "drinks"];
 
 export const products = {
   all: [
     {
-      src: null,
+      src: burgers1,
       name: "Original Stack",
       description:
         "A double layer of juicy beef patties, topped with cheddar and a dollop of our house sauce.",
@@ -12,67 +34,130 @@ export const products = {
   ],
   burgers: [
     {
-      src: null,
+      src: burgers1,
       name: "Original Stack",
       description:
         "A double layer of juicy beef patties, topped with cheddar and a dollop of our house sauce.",
       price: "8,99€",
     },
     {
-      src: null,
+      src: burgers2,
       name: "BBQ Bacon Burger",
       description:
         "Savory beef burger under a blanket of crispy bacon, smothered in BBQ sauce and cheddar.",
       price: "9,49€",
     },
     {
-      src: null,
+      src: burgers3,
       name: "Vegan Truffle",
       description:
         "A Beyond burger with a hint of truffle in a creamy veganise, simple yet sophisticated.",
       price: "9,99€",
     },
+    {
+      src: burgers4,
+      name: "Iberian Deluxe",
+      description:
+        "Luxurious double beef burger, complemented by the rich flavor of Iberian ham and smooth cheddar.",
+      price: "10,49€",
+    },
+    {
+      src: burgers5,
+      name: "Avocado Vegan Dream",
+      description:
+        "A Beyond burger creation, featuring vegan mozzarella and ripe avocado for a fresh twist.",
+      price: "9,49€",
+    },
+    {
+      src: burgers6,
+      name: "Bacon Cheddar Crunch",
+      description:
+        "Classic beef burger paired with crunchy bacon and a generous helping of cheddar.",
+      price: "8,99€",
+    },
+    {
+      src: burgers7,
+      name: "Truffled Egg Burger",
+      description:
+        "An indulgent beef burger topped with a rich truffle sauce and crowned with a fried egg.",
+      price: "10,99€",
+    },
+    {
+      src: burgers8,
+      name: "Classic Cheddar Melt",
+      description:
+        "A comforting double beef burger, fully dressed in cheddar and our special sauce.",
+      price: "8,49€",
+    },
+    {
+      src: burgers9,
+      name: "Solo Burger Bliss",
+      description:
+        "A simple yet satisfying single beef patty burger, accented with cheddar and tangy pickles.",
+      price: "7,49€",
+    },
   ],
   sides: [
     {
-      src: null,
+      src: sides1,
       name: "Wings of Glory",
       description:
         "Succulent chicken wings, upsized for the ultimate poultry aficionado. When it’s time to soar, make it with these wings.",
       price: "5,99€",
     },
     {
-      src: null,
+      src: sides2,
       name: "Golden Nuggets",
       description:
         "6 crispy, golden chicken nuggets, perfect for dipping into our rich, flavorful sauce.",
       price: "4,99€",
     },
     {
-      src: null,
+      src: sides3,
       name: "Supreme Spuds",
       description:
-        "Elevate your meal with our Supreme Potatoes, and don’t miss out on trying them with our new Sour Cream sauce.",
+        "Elevate your meal with our Supreme Potatoes, and don't miss out on trying them with our new Sour Cream sauce.",
       price: "3,99€",
+    },
+    {
+      src: sides4,
+      name: "Chili Cheddar Temptations",
+      description:
+        "Indulge in our new Chili Cheese Bites, bursting with cheddar and jalapeños for that extra kick.",
+      price: "4,49€",
+    },
+    {
+      src: sides5,
+      name: "Crispy Chicken Delicacies",
+      description:
+        "Savor the crunch with our bite-sized battered chicken delights. Chicken McBites, just the way you like them.",
+      price: "5,49€",
+    },
+    {
+      src: sides6,
+      name: "Onion Ring Olympus",
+      description:
+        "Ascend to onion bliss with our Onion Rings. Perfect as a starter or side, for sharing or solo indulgence, they’re round, tasty, and crunchy.",
+      price: "3,49€",
     },
   ],
   desserts: [
     {
-      src: null,
+      src: desserts1,
       name: "Caramel Bliss Sundae",
       description:
         "Indulge in the sweet embrace of vanilla ice cream drizzled with caramel syrup.",
       price: "4,99€",
     },
     {
-      src: null,
+      src: desserts2,
       name: "Strawberry Classic Scoop",
       description:
         "Timeless vanilla ice cream meets strawberry syrup for a match made in dessert heaven.",
       price: "4,99€",
     },
     {
-      src: null,
+      src: desserts3,
       name: "Chocolate Dream Drizzle",
       description:
         "Vanilla ice cream paired with chocolate syrup, a top-tier classic treat.",
@@ -81,21 +166,21 @@ export const products = {
   ],
   drinks: [
     {
-      src: null,
+      src: drinks1,
       name: "Coca-Cola",
       description:
         "The iconic Coca-Cola, served ice-cold for that refreshing fizz",
       price: "2,49€",
     },
     {
-      src: null,
+      src: drinks2,
       name: "Monster Energy",
       description:
         "Unleash the beast with Monster Energy, for when you need that extra kick.",
       price: "3,49€",
     },
     {
-      src: null,
+      src: drinks3,
       name: "Mineral Water",
       description: "Crisp and refreshing mineral water to quench your thirst.",
       price: "1,99€",
